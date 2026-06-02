8cf8b373bbbc73a69dd3772fcaff9c6c6a6ac103
Da Def <da.def.lang.belta@gmail.com>
2026-06-02 15:39:49 +0200

    .

--- server.py
+++ server.py
@@ -3,13 +3,46 @@ from flask import Flask, request, send_from_directory
 
 app = Flask(__name__)
 
+def mkstate(name,args,clauses):
+    return "::".join([name,args]+clauses)
+
+
 @app.post('/fn')
 def dfunction():
-    return f'''
-    <!doctype html>
-    <title>Define Function</title>
-    <h1>{request.form['name']}({request.form['args']})</h1>
-    '''
+    if 'name' in request.form:
+        return f'''
+        <!doctype html>
+        <title>Define Function</title>
+        <h1>{request.form['name']}({request.form['args']})</h1>
+        <form action="/fn" method=post>
+            <input type=hidden name=state value="{mkstate(
+                request.form['name'], request.form['args'],[])}">
+            <input type=text name=cond>
+            <input type=text name=val>
+            <input type=submit value=Define>
+        </form>
+        '''
+    else:
+        vs = request.form['state'].split('::')
+        name, args = vs[:2]
+        clauses = vs[2:]
+        clauses.append(request.form['cond']+" ⟼  "+request.form['val'])
+        return (f'''
+        <!doctype html>
+        <hr>
+        {name}({args})
+        <ul>'''+
+        ''.join(f'<li>{c}' for c in clauses)+
+        f'''</ul>
+        <hr>
+        <form action="/fn" method=post>
+            <input type=hidden name=state value="{mkstate(
+                name, args, clauses)}">
+            <input type=text name=cond>
+            <input type=text name=val>
+            <input type=submit value=Define>
+        </form>
+        ''')
 
 @app.route('/')
 def homepage():
